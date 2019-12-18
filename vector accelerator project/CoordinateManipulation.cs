c7553d4799b8477cbc2a7fb82172f5a57de5ab63
patkon <patrikon2-c@my.cityu.edu.hk>
2019-12-18 16:52:14 +0800

    18/12/19 pre-PNA tidy

--- vector accelerator project/CoordinateManipulation.cs
+++ vector accelerator project/CoordinateManipulation.cs
@@ -1,12 +1,209 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace vector_accelerator_project
 {
     class CoordinateManipulation
     {
     }
+
+    public class Movement_variables
+    {
+        //Variables that store coordinates:
+        public int[] start_position;
+        public int[] end_position;
+        //a list of integer arrays, each with 2 elements:
+        public List<int[]> intermediate_positions;
+
+        //Variables that store other parameters:
+        public int drop_by;  //axis-c drop by how many units while sampling
+        //private int[] sample_units = new int[2] { 0, 0 }; //sample every "sample_units" units
+        public int axis_c_rest_position ;
+
+        public int increment_unit;
+        public int speed_a; //recommended speed
+        public int speed_b; //recommended speed
+        public int speed_c; //recommended speed
+
+
+        public Movement_variables()
+        {
+            //Variables that store coordinates:
+            start_position = new int[2] { 0, 0 };
+            end_position = new int[2] { 0, 0 };
+            //a list of integer arrays, each with 2 elements:
+            intermediate_positions = null;
+
+            //Variables that store other parameters:
+            drop_by = 0;  //axis-c drop by how many units while sampling
+            //private int[] sample_units = new int[2] { 0, 0 }; //sample every "sample_units" units
+            axis_c_rest_position = 0;
+
+
+            increment_unit = 10000;
+            speed_a = 5000; //recommended speed
+            speed_b = 5000; //recommended speed
+            speed_c = 400000; //recommended speed
+        }
+    }
+
+    // initially was planning to use this for special_manual_movement and so on, but might be removed now instead:
+    partial class Form1 {
+        class Movement_util
+        {
+            private Form1 form;
+            private gclib gclib;
+
+            public Movement_util(Form1 form, gclib gclib)
+            {
+                this.form = form;
+                this.gclib = gclib;
+            }
+            
+            //Basic PA movement, while printing output to TextBox1:
+            private void runAbsoluteMoveCommand(string axis, int distance_units, int speed)
+            {
+                try
+                {   
+                    form.printTextBox1("Preparing " + axis + " axis for PA movement. This could cause errors if the axis is not initialized...");
+                    gclib.GCommand("AB;MO;SH" + axis);
+                    //compound commands are possible though typically not recommended
+                    form.printTextBox1("Ok");
+                    gclib.GCommand("PA" + axis + "=" + distance_units);
+
+                    //might implement speed control parameter in future
+                    gclib.GCommand("SP" + axis + "=" + speed);
+                    form.printTextBox1("Profiling a move on axis" + axis + "... ");
+                    gclib.GCommand("BG" + axis);
+                    form.printTextBox1("Waiting for motion to complete... ");
+                    gclib.GMotionComplete(axis);
+                    form.printTextBox1("done");
+                    //update absolute position and display in textBox2:
+                    //cur_abs_pos(abs_position);  // as stated in Master, this is commented as it has been identified as the source of neg move problem
+                }
+                catch (Exception ex)
+                {
+                    form.printTextBox1("ERROR in runAbsoluteMoveCommand on axis " + axis + ": " + ex.Message, Form1.PrintStyle.Instruction);
+                }
+            }
+
+            //Simplifier function used for function proceeding this:
+            private void special_move_helper(int [] position, Movement_variables move_vars)
+            {
+                int dropped_abs_position = move_vars.drop_by + move_vars.axis_c_rest_position;
+                runAbsoluteMoveCommand("C", move_vars.axis_c_rest_position, move_vars.speed_c);
+                runAbsoluteMoveCommand("A", position[0], move_vars.speed_a);
+                runAbsoluteMoveCommand("B", position[1], move_vars.speed_b);
+                runAbsoluteMoveCommand("C", dropped_abs_position, move_vars.speed_c);
+            }
+        }
+    }
+
+    partial class Form1
+    {
+        public delegate void runAbsoluteMoveCommand(string axis, int distance_units, int speed);
+        public delegate void special_move_helper(int[] position, Movement_variables move_vars);
+
+        class MovementType
+        {
+            protected PNA analyzer;
+            //protected gclib gclib;
+            public MovementType(PNA analyzer)
+            {
+                this.analyzer = analyzer;
+                //this.gclib = gclib;
+            }
+
+
+            public virtual void move(runAbsoluteMoveCommand runAbsoluteMoveCommand, Movement_variables move_vars) { }
+            
+        }
+
+        class ManualMovement : MovementType
+        {
+            public ManualMovement(PNA analyzer)
+                : base(analyzer)
+            {
+
+            }
+
+            override
+            public void move(runAbsoluteMoveCommand runAbsoluteMoveCommand, special_move_helper special_move_helper, Movement_variables move_vars)
+            {   
+                special_move_helper(move_vars.start_position, move_vars);
+                // BLOCK of code to complete user specified task....
+                // I replace it temporarily with a simple pause:
+
+                // i add VNA stuff in now: 
+                analyzer.PNA_scan(move_vars.start_position, move_vars.drop_by);
+                                                            //WANT TO PASS PNA_SCAN ARGUMENTS TO BE ACCESSED WITHIN THAT METHOD ITSELF
+                System.Threading.Thread.Sleep(200);
+                move_vars.intermediate_positions?.ForEach(a => {      
+                    special_move_helper(a, move_vars);
+                    // BLOCK of code to complete user specified task....
+                    // I replace it temporarily with a simple pause:
+                    System.Threading.Thread.Sleep(200);
+                
+                    // i add VNA stuff in now:
+                    analyzer.PNA_scan(a, move_vars.drop_by);
+                });
+
+                special_move_helper(move_vars.end_position, move_vars);
+                // BLOCK of code to complete user specified task....
+                // I replace it temporarily with a simple pause:
+                System.Threading.Thread.Sleep(200);
+
+                // i add VNA stuff in now:
+                PNA_scan(move_vars.end_position, move_vars.drop_by);
+
+                // return to original axis-c rest position before ending movement:
+                runAbsoluteMoveCommand("C", move_vars.axis_c_rest_position, move_vars.speed_c);
+            }   
+        }
+
+        class SegmentMovement : MovementType
+        {
+
+        }
+
+
+        // All execution comes from these below classes, the above classes are utilities for these classes only:
+
+        class Movement
+        {
+
+            public Movement()
+            {
+
+            }
+
+            public void move(MovementType a)
+            {
+                a.move();
+            }
+
+        }
+
+
+        class Stepper : Movement
+        {
+            public void move()
+            {
+
+            }
+        }
+
+        class Mm : Movement
+        {
+
+        }
+
+
+    }
 }
+
---
+++ vector accelerator project/Movement.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vector_accelerator_project
+{   
+    class MovementVariables
+    {
+        //Variables that store coordinates: 
+        public virtual int[] Start_position { get; private set; }
+        public virtual int[] End_position { get; private set; }
+        //a list of integer arrays, each with 2 elements:
+        public virtual List<int[]> Intermediate_positions { get; private set; }
+
+        // Used for Segment Movement:
+        // each int[] has 6 elements (in-order): 
+        // a(start), a(end), a(delta), b(start), b(end), b(delta)
+        public List<int[]> Segment_positions { get; private set; }
+    
+
+        //Variables that store other parameters:
+        public virtual int Axis_c_drop_by { get; set; }  //axis-c drop by how many units while sampling
+        public virtual int Axis_c_rest_position { get; set; }
+
+        public virtual int Increment_unit { get; set; }
+        public virtual int Speed_a { get; set; } //recommended speed
+        public virtual int Speed_b { get; set; } //recommended speed
+        public virtual int Speed_c { get; set; } //recommended speed
+
+        public MovementVariables()
+        {
+            //Variables that store coordinates:
+            Start_position = new int[2] { 0, 0 };
+            End_position = new int[2] { 0, 0 };
+            //a list of integer arrays, each with 2 elements:
+            Intermediate_positions = new List<int[]>();
+
+            Segment_positions = new List<int[]>();
+
+            //Variables that store other parameters:
+            Axis_c_drop_by = 0;  //axis-c drop by how many units while sampling
+            Axis_c_rest_position = 0;
+
+            Increment_unit = 10000;
+            Speed_a = 5000; //recommended speed
+            Speed_b = 5000; //recommended speed
+            Speed_c = 400000; //recommended speed
+        }
+
+        public virtual bool set_StartPosition(int index, int value) { return true; }
+        public virtual bool set_EndPosition(int index, int value) { return true; }
+        public virtual bool set_IntermediatePosition(int index, int value) { return true; }
+        public virtual bool set_SegmentPosition(int index, int value) { return true; }
+
+        public delegate void displayFunc(); 
+
+        public bool add_Segment(displayFunc display)
+        {
+            // check if Segment values are valid (i.e. if they add up):
+            bool same_num_samples = false;
+            int b_sample_num = (Math.Abs(Segment_positions.Last()[5]) > 0) ? (Segment_positions.Last()[4] - Segment_positions.Last()[3]) / Segment_positions.Last()[5] : 0;
+            int a_sample_num = (Math.Abs(Segment_positions.Last()[2]) > 0) ? (Segment_positions.Last()[1] - Segment_positions.Last()[0]) / Segment_positions.Last()[2] : 0;
+            same_num_samples = (b_sample_num == 0 || a_sample_num == 0 || (b_sample_num != a_sample_num)) ? false : true;
+            if (same_num_samples && ((Segment_positions.Last()[1] - Segment_positions.Last()[0]) % Segment_positions.Last()[2] == 0) && ((Segment_positions.Last()[4] - Segment_positions.Last()[3]) % Segment_positions.Last()[5] == 0))
+            {
+                Segment_positions.Add(new int[6] { 0, 0, 0, 0, 0, 0 });
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Segment values do not add up. Check your input values.", "Invalid Input.");
+                return false;
+            }
+            display();
+            return true;     
+        }
+
+    }
+
+    class MovementVariables_stepperUnit : MovementVariables
+    {
+        public MovementVariables_stepperUnit()
+            : base()
+        {
+        }
+
+        public override bool set_StartPosition(int index, int value)
+        {
+            try
+            {
+                Start_position[index] = value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+        public override bool set_EndPosition(int index, int value)
+        {
+            try
+            {
+                End_position[index] = value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+        public override bool set_IntermediatePosition(int index, int value)
+        {
+            try
+            {
+                Intermediate_positions.Last()[index] = value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+        public override bool set_SegmentPosition(int index, int value)
+        {
+            try
+            {
+                Segment_positions.Last()[index] = value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    class MovementVariables_mmUnit : MovementVariables
+    {
+        public MovementVariables_mmUnit()
+            : base()
+        {
+        }
+
+        //Variables that store other parameters:
+        private int axis_c_drop_by;
+        private int axis_c_rest_position;
+        public override int Axis_c_drop_by
+        {
+            get => axis_c_drop_by;
+            set => axis_c_drop_by = value * 14970;
+
+        }  
+        public override int Axis_c_rest_position
+        {
+            get => axis_c_rest_position;
+            set => axis_c_rest_position = value * 14970;
+        }
+
+        public override int Increment_unit { get; set; }
+
+        public override bool set_StartPosition(int index, int value)
+        {
+            try
+            {
+                Start_position[index] = value * 207;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+        public override bool set_EndPosition(int index, int value)
+        {
+            try
+            {
+                End_position[index] = value * 207;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+        public override bool set_IntermediatePosition(int index, int value)
+        {
+            try
+            {
+                Intermediate_positions.Last()[index] = value * 207;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+        public override bool set_SegmentPosition(int index, int value)
+        {
+            try
+            {
+                Segment_positions.Last()[index] = value * 207;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic exception handler: {e}");
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
+    class MoveFactory
+    {
+        private Form1 form;
+        private gclib gclib;
+
+        public MoveFactory(Form1 form, gclib gclib)
+        {
+            this.form = form;
+            this.gclib = gclib;
+        }
+
+
+        //Basic PA movement, while printing output to TextBox1:
+        protected internal void runAbsoluteMoveCommand(string axis, int distance_units, int speed)
+        {
+            try
+            {
+                form.printTextBox1("Preparing " + axis + " axis for PA movement. This could cause errors if the axis is not initialized...");
+                gclib.GCommand("AB;MO;SH" + axis);
+                //compound commands are possible though typically not recommended
+                form.printTextBox1("Ok");
+                gclib.GCommand("PA" + axis + "=" + distance_units);
+
+                //might implement speed control parameter in future
+                gclib.GCommand("SP" + axis + "=" + speed);
+                form.printTextBox1("Profiling a move on axis" + axis + "... ");
+                gclib.GCommand("BG" + axis);
+                form.printTextBox1("Waiting for motion to complete... ");
+                gclib.GMotionComplete(axis);
+                form.printTextBox1("done");
+                //update absolute position and display in textBox2:
+                //cur_abs_pos(abs_position);  // as stated in Master, this is commented as it has been identified as the source of neg move problem
+            }
+            catch (Exception ex)
+            {
+                form.printTextBox1("ERROR in runAbsoluteMoveCommand on axis " + axis + ": " + ex.Message, Form1.PrintStyle.Instruction);
+            }
+        }
+        //Note to self: axis must be in Capital letters.
+        //Simple PR movement:
+        public void runRelativeMoveCommand(string axis, int distance_units, int speed)
+        {
+            try
+            {
+                form.printTextBox1("Preparing " + axis + " axis for PR movement. This could cause errors if the axis is not initialized...");
+                gclib.GCommand("AB;MO;SH" + axis);
+                //compound commands are possible though typically not recommended
+                form.printTextBox1("Ok");
+                gclib.GCommand("PR" + axis + "=" + distance_units);
+
+                //might implement speed control parameter in future
+                gclib.GCommand("SP" + axis + "=" + speed);
+                form.printTextBox1("Profiling a move on axis" + axis + "... ");
+                gclib.GCommand("BG" + axis);
+                form.printTextBox1("Waiting for motion to complete... ");
+                gclib.GMotionComplete(axis);
+                form.printTextBox1("done");
+                //update absolute position and display in textBox2:
+                //cur_abs_pos(abs_position);  // as stated in Master, this is commented as it has been identified as the source of neg move problem
+            }
+            catch (Exception ex)
+            {
+                form.printTextBox1("ERROR in runRelativeMoveCommand on axis " + axis + ": " + ex.Message, Form1.PrintStyle.Instruction);
+            }
+        }
+
+        //Movement for a specific set of AB-axis coordinates within MovementVariables:
+        protected internal void special_move_helper(int[] position, MovementVariables movementVariables)
+        {
+            int dropped_abs_position = movementVariables.Axis_c_drop_by + movementVariables.Axis_c_rest_position;
+            runAbsoluteMoveCommand("C", movementVariables.Axis_c_rest_position, movementVariables.Speed_c);
+            runAbsoluteMoveCommand("A", position[0], movementVariables.Speed_a);
+            runAbsoluteMoveCommand("B", position[1], movementVariables.Speed_b);
+            runAbsoluteMoveCommand("C", dropped_abs_position, movementVariables.Speed_c);
+        }
+    }
+
+    class Form1_EventListeners
+    {
+        private Form1 form;
+
+        public Form1_EventListeners(Form1 form)
+        {
+            this.form = form;
+        }
+
+        internal Boolean mmButton_checked()
+        {
+            return form.mmButton_checked();
+        }
+
+        //internal void clearMovementValueDisplay()
+        //{
+       //     form.clearMovementValue_textBox();
+       // }
+
+    }
+
+    // Currently supports both manual and segmented movement:
+    class MovementType
+    {
+        protected MovementVariables movementVariables;
+        protected PNA analyzer;
+        protected MoveFactory moveFactory;
+        protected Form1_EventListeners form1_EventListeners;
+        
+        public MovementType(PNA analyzer, Form1 form, gclib gclib, MovementVariables movementVariables)
+        {
+            this.movementVariables = movementVariables;
+            this.analyzer = analyzer;
+            this.moveFactory = new MoveFactory(form, gclib);
+            this.form1_EventListeners = new Form1_EventListeners(form);
+            //this.gclib = gclib;
+        }
+
+        public virtual void move() { }
+        //public virtual void displayFunc() { } // Format for displaying your movement required values
+    }
+    
+    class ManualMovement : MovementType
+    {
+        public ManualMovement(PNA analyzer, Form1 form, gclib gclib, MovementVariables movementVariables)
+            : base(analyzer, form, gclib, movementVariables)
+        {
+        }
+
+        override
+        public void move()
+        {
+            moveFactory.special_move_helper(movementVariables.Start_position, movementVariables);
+            // BLOCK of code to complete user specified task....
+            // I replace it temporarily with a simple pause:
+
+            // i add VNA stuff in now: 
+            analyzer.PNA_scan(movementVariables.Start_position, movementVariables.Axis_c_drop_by);
+            //WANT TO PASS PNA_SCAN ARGUMENTS TO BE ACCESSED WITHIN THAT METHOD ITSELF
+            System.Threading.Thread.Sleep(200);
+            movementVariables.Intermediate_positions?.ForEach(a => {
+                moveFactory.special_move_helper(a, movementVariables);
+                // BLOCK of code to complete user specified task....
+                // I replace it temporarily with a simple pause:
+                System.Threading.Thread.Sleep(200);
+
+                // i add VNA stuff in now:
+                analyzer.PNA_scan(a, movementVariables.Axis_c_drop_by);
+            });
+
+            moveFactory.special_move_helper(movementVariables.End_position, movementVariables);
+            // BLOCK of code to complete user specified task....
+            // I replace it temporarily with a simple pause:
+            System.Threading.Thread.Sleep(200);
+
+            // i add VNA stuff in now:
+            PNA_scan(movementVariables.End_position, movementVariables.Axis_c_drop_by);
+
+            // return to original axis-c rest position before ending movement:
+            moveFactory.runAbsoluteMoveCommand("C", movementVariables.Axis_c_rest_position, movementVariables.Speed_c);
+        }
+
+
+    }
+
+    class SegmentMovement : MovementType
+    {
+        public SegmentMovement(PNA analyzer, Form1 form, gclib gclib, MovementVariables movementVariables)
+            : base(analyzer, form, gclib, movementVariables)
+        {
+        }
+
+        override
+        public void move()
+        {
+            int counter = 0; // indicates how many segments have been processed.
+            movementVariables.Segment_positions?.ForEach(a =>
+            {
+                int multiplier = 0;
+                // Do not consider last element of segment_positions as it does not contain validated input that has passed through the function button21_clicked:
+                if (counter < movementVariables.Segment_positions.Count - 1)
+                {
+                    while (true)
+                    {
+                        movementVariables.Start_position[0] = multiplier * a[2] + a[0];
+                        movementVariables.Start_position[1] = multiplier * a[5] + a[3];                                 
+                        multiplier += 1;
+                        //if ( (start_position[0] < 0 && (Math.Abs(start_position[0]) > Math.Abs(a[1])  || Math.Abs(start_position[1]) > Math.Abs(a[4])) ) || (start_position[0] < 0 && (Math.Abs(start_position[0]) > Math.Abs(a[1]) || Math.Abs(start_position[1]) > Math.Abs(a[4])))) break;
+                        moveFactory.special_move_helper(movementVariables.Start_position, movementVariables);
+
+                        // i add VNA stuff in now:
+                        PNA_scan(movementVariables.Start_position, movementVariables.Axis_c_drop_by);
+
+                        if (movementVariables.Start_position[0] == a[1] || movementVariables.Start_position[1] == a[4]) break;                      
+                    }
+                    counter += 1;
+                }
+            });
+            // return to original axis-c rest position before ending movement:
+            moveFactory.runAbsoluteMoveCommand("C", movementVariables.Axis_c_rest_position, movementVariables.Speed_c);
+        }
+    }
+
+    public class Movement
+    {
+        public Movement()
+        {
+
+        }
+    }
+
+    
+}
